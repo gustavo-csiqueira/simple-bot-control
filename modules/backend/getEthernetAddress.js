0ca709dac5ac8af5c7e2c3884f6a92b2ee0b14ab
Gustavo Carvalho de Siqueira <gustavo.csiqueira1806@gmail.com>
2025-07-18 17:54:52 -0300

    Add server config file
    * Server config file added
    
    * Server config file really added
    
    * Server config file finished

--- modules/backend/getEthernetAddress.js
+++ modules/backend/getEthernetAddress.js
@@ -1,11 +1,14 @@
+import { readFileSync } from 'fs'
 import {networkInterfaces} from 'os'
 
-export default function getEthernetAddress() {
-    const ETHERNET_INTERFACE_NAME = "enp2s0";
+export default function getEthernetAddress(config_path) {
+    const configs = JSON.parse(readFileSync(config_path, 'utf-8'))
+
+    const ETHERNET_INTERFACE_NAME = configs["ethernet-interface"];
     const interfaces = networkInterfaces();
     const ethernetInterface = interfaces[ETHERNET_INTERFACE_NAME];
 
-    if(!ethernetInterface) return "127.0.0.1"
+    if(!ethernetInterface) return "127.0.0.1" 
 
     for (let e of ethernetInterface) {
         if (e["family"] == "IPv4")
